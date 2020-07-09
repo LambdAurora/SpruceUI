cfc6840fb9f0fee9567eaf936a92b572a630a248
LambdAurora <aurora42lambda@gmail.com>
2020-07-09 18:11:19 +0200

    SpruceUI v1.5.7: Add generic texts.

---
+++ src/main/java/me/lambdaurora/spruceui/SpruceTexts.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright © 2020 LambdAurora <aurora42lambda@gmail.com>
+ *
+ * This file is part of SpruceUI.
+ *
+ * Licensed under the MIT license. For more information,
+ * see the LICENSE file.
+ */
+
+package me.lambdaurora.spruceui;
+
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
+
+/**
+ * Represents a text utility class.
+ *
+ * @author LambdAurora
+ * @version 1.5.7
+ * @since 1.5.7
+ */
+public final class SpruceTexts
+{
+    /**
+     * Represents the option value "default" as text.
+     */
+    public static final Text OPTIONS_GENERIC_DEFAULT = new TranslatableText("spruceui.options.generic.default");
+
+    /**
+     * Represents the option value "fancy" as text.
+     */
+    public static final Text OPTIONS_GENERIC_FANCY = new TranslatableText("spruceui.options.generic.fancy");
+
+    /**
+     * Represents the option value "fast" as text.
+     */
+    public static final Text OPTIONS_GENERIC_FAST = new TranslatableText("spruceui.options.generic.fast");
+
+    /**
+     * Represents the option value "fastest" as text.
+     */
+    public static final Text OPTIONS_GENERIC_FASTEST = new TranslatableText("spruceui.options.generic.fastest");
+
+    /**
+     * Represents the option value "none" as text.
+     */
+    public static final Text OPTIONS_GENERIC_NONE = new TranslatableText("spruceui.options.generic.none");
+
+    /**
+     * Represents the option value "simple" as text.
+     */
+    public static final Text OPTIONS_GENERIC_SIMPLE = new TranslatableText("spruceui.options.generic.simple");
+
+    /**
+     * Represents the option value "unbound" as text.
+     */
+    public static final Text OPTIONS_GENERIC_UNBOUND = new TranslatableText("spruceui.options.generic.unbound");
+}
