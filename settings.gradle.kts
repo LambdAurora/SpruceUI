93f25d6a649660d8108eb4660c285a0e6f2376dd
LambdAurora <email@lambdaurora.dev>
2025-03-23 19:48:56 +0100

    Use lambdamcdev plugin in buildscript.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,14 @@
 pluginManagement {
 	repositories {
+		gradlePluginPortal()
 		maven {
 			name = "Fabric"
 			url = uri("https://maven.fabricmc.net/")
 		}
-		gradlePluginPortal()
+		maven {
+			name = "Gegy"
+			url = uri("https://maven.gegy.dev/releases/")
+		}
 	}
 }
 
